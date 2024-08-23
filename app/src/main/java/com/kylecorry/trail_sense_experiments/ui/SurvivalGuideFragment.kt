69c4ee58e584510944ebb1753c46f4bacad32387
Kyle Corry <kylecorry31@gmail.com>
2024-08-23 15:36:02 -0400

    Compress guide

--- app/src/main/java/com/kylecorry/trail_sense_experiments/ui/SurvivalGuideFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense_experiments/ui/SurvivalGuideFragment.kt
@@ -11,7 +11,12 @@ import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
+import com.kylecorry.andromeda.files.AssetFileSystem
+import com.kylecorry.andromeda.files.LocalFileSystem
+import com.kylecorry.andromeda.files.ZipUtils
 import com.kylecorry.andromeda.fragments.BoundFragment
+import com.kylecorry.andromeda.fragments.inBackground
+import com.kylecorry.luna.coroutines.onMain
 import com.kylecorry.trail_sense_experiments.databinding.FragmentSurvivalGuideBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,12 +24,21 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SurvivalGuideFragment : BoundFragment<FragmentSurvivalGuideBinding>() {
 
+    private val assets by lazy { AssetFileSystem(requireContext()) }
+    private val localFiles by lazy { LocalFileSystem(requireContext()) }
+
     @SuppressLint("ClickableViewAccessibility", "MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val webviewAssetLoader = WebViewAssetLoader.Builder()
-            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(requireContext()))
+            .addPathHandler(
+                "/assets/",
+                WebViewAssetLoader.InternalStoragePathHandler(
+                    requireContext(),
+                    requireContext().filesDir
+                )
+            )
             .build()
 
         binding.webView.webViewClient = object : WebViewClientCompat() {
@@ -47,9 +61,30 @@ class SurvivalGuideFragment : BoundFragment<FragmentSurvivalGuideBinding>() {
 
         // Set default background color of the webview
         binding.webView.setBackgroundColor(Color.TRANSPARENT)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        inBackground {
+            if (localFiles.getDirectory("survival-guide", false).exists()) {
+                onMain {
+                    openGuide()
+                }
+                return@inBackground
+            }
+            val zip = assets.stream("survival-guide.zip")
+            ZipUtils.unzip(zip, localFiles.getDirectory("survival-guide"), 300)
+            onMain {
+                openGuide()
+            }
+        }
+
+    }
+
+    private fun openGuide() {
         // It doesn't actually make a network request, this just makes it use the webview asset loader
-        binding.webView.loadUrl("https://appassets.androidplatform.net/assets/survival_guide/guide.html")
+        binding.webView.loadUrl("https://appassets.androidplatform.net/assets/survival-guide/guide.html")
     }
 
     override fun generateBinding(
